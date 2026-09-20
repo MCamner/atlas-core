aabbce490697854cd2bb6e6f405f6f4bd9e25062
Mattias Camner <mattias.camner@gmail.com>
2026-09-20 22:54:21 +0200

    feat(p03): expose explicit hard-isolated run API

--- atlas_core/__init__.py
+++ atlas_core/__init__.py
@@ -2,6 +2,7 @@
 
 from .controller import AtlasController
 from .evidence_base import EvidenceBase
+from .isolation import run_isolated
 from .machine import STATE_MACHINE_VERSION, STOP_REASONS, StopClass
 from .state import AtlasEvaluation, AtlasPlan, AtlasRoute, AtlasRunState
 from .adapters.model import ModelAdapter, ModelResult, StubModelAdapter
@@ -9,6 +10,7 @@ from .adapters.model import ModelAdapter, ModelResult, StubModelAdapter
 __all__ = [
     "AtlasController",
     "EvidenceBase",
+    "run_isolated",
     "AtlasEvaluation",
     "AtlasPlan",
     "AtlasRoute",
